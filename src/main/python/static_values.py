605209439da624def0857a2903f9a69e76d4ae03
camillo2008 <jordancaminati@libero.it>
2021-07-01 04:31:28 -0700

    Added files for static values

---
+++ src/main/python/static_values.py
@@ -0,0 +1,14 @@
+# Certs for ESP32s
+# The certificates are .der format
+CERT_FILE = "/flash/cert"
+KEY_FILE = "/flash/key"
+
+# if you change the ClientId make sure update AWS policy
+MQTT_CLIENT_ID = "COGLIONE"
+MQTT_PORT = 8883
+
+# if you change the topic make sure update AWS policy
+MQTT_TOPIC = b"TESTONE"
+
+DEF_MAX_WATER_LVL = 10000
+DEF_MIN_WATER_LVL = 1000
